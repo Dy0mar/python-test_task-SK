5815008a484ea2cb4fa50a7859f4f68cc3558f66
Dy0mar <kurtudins@gmail.com>
2017-05-31 13:33:58 +0300

    edit slug

--- product/models.py
+++ product/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
-    slug = models.CharField(max_length=255)
+    slug = models.CharField(max_length=255, unique=True, blank=False, null=False)
     description = models.TextField()
 
     def __str__(self):
@@ -12,7 +12,7 @@ class Category(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=100)
+    slug = models.SlugField(max_length=255, unique=True, blank=False, null=False)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
